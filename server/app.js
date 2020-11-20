06a31cbe1c2b763c0196de36f089bebf94186911
Paul Okello <paulokello273@gmail.com>
2020-11-20 13:13:16 +0300

    set up first middleware

--- server/app.js
+++ server/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+//Set up middleware
+app.use(
+  "/graphql",
+  qraphqlHTTP({
+    schema,
+  })
+);
+
 app.listen(4000, () => {
   console.log("Now listening for requests on port 4000");
 });
