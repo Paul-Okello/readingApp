4fc486dfe9a9dda18760dbeb4cf7bbcd50cd30fe
Paul Okello <paulokello273@gmail.com>
2020-11-20 14:09:54 +0300

    type relations

--- server/Schema/schema.js
+++ server/Schema/schema.js
@@ -1,13 +1,39 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
+const books = [
+  { name: "All Or Nothing", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "Final Dance", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "Twice As Tall", genre: "Fantasy", id: "3", authorId: "3" },
+  { name: "Available", genre: "Fantasy", id: "4", authorId: "4" },
+];
+const authors = [
+  { name: "John Bull", age: 44, id: "1" },
+  { name: "Jeanie Buss", age: 34, id: "2" },
+  { name: "Joseph Leting", age: 87, id: "3" },
+  { name: "Louis Ceaser", age: 74, id: "4" },
+];
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        console.log(parent);
+        return _.find(authors, { id: parent.authorId });
+      },
+    },
   }),
 });
 const AuthorType = new GraphQLObjectType({
@@ -38,4 +64,8 @@ const RootQuery = new GraphQLObjectType({
       },
     },
   },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
