c2913de6fffb69bcbaf950b2089655a92f786ea8
Paul Okello <paulokello273@gmail.com>
2020-11-21 11:26:43 +0300

    addBook Mutation

--- server/Schema/schema.js
+++ server/Schema/schema.js
@@ -100,8 +100,17 @@ const Mutation = new GraphQLObjectType({
           name: args.name,
           age: args.age,
         });
-        author.save();
+        return author.save();
       },
+    },
+  },
+  addBook: {
+    type: BookType,
+    args: {
+      name: { type: GraphQLString },
+      genre: { type: GraphQLString },
+      authorId: { type: GraphQLID },
+    },
     },
   },
 });
