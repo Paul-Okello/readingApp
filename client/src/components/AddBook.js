3cdf27cb01dd3f4f37b1c01c4aafec19ea9ecd9e
Paul Okello <paulokello273@gmail.com>
2020-11-21 16:22:27 +0300

    AddBook HTML

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function AddBook() {
-  return <div></div>;
+  const { loading, data, error } = useQuery(GET_AUTHORS_QUERY);
+  return (
+    <form action="" id="add__book">
+      <div className="field">
+        <label htmlFor="">Book Name</label>
+        <input type="text" />
+      </div>
+      <div className="field">
+        <label htmlFor="">Genre</label>
+        <input type="text" />
+      </div>
+      <div className="field">
+        <label htmlFor="">Author</label>
+        <select name="" id="">
+          <option value="">Select Author</option>
+        </select>
+      </div>
+    </form>
+  );
 }
 
 export default AddBook;
