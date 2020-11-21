99fab918fe50821db537c3a2044a1e14098b2bf9
Paul Okello <paulokello273@gmail.com>
2020-11-21 17:49:15 +0300

    addBook mutation updated

--- client/src/querries/querries.js
+++ client/src/querries/querries.js
@@ -17,8 +17,8 @@ export const GET_AUTHORS_QUERY = gql`
   }
 `;
 export const ADD_BOOK_MUTATION = gql`
-  mutation {
-    addBook(name: "", genre: "", authorId: "") {
+  mutation($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
       name
       id
     }
