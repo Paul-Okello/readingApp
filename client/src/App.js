95831a3ad5d33b2310e9f05288e82abb110f2ea0
Paul Okello <paulokello273@gmail.com>
2020-11-21 13:15:18 +0300

    apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import BookList from "./components/BookList";
 
+//apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <div id="main">
