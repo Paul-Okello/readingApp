2f3bcb7047f630c7777afd72ec9d5b33aad0c6ca
Paul Okello <paulokello273@gmail.com>
2020-11-21 10:42:02 +0300

    mongoDB connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const { MONGODB } = require("./config");
 const schema = require("./Schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
-
+//Database
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  });
 //Set up middleware
 app.use(
   "/graphql",
