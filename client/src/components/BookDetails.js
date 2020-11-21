2681ae181a151d2a8c40816054fca9554c12fddd
Paul Okello <paulokello273@gmail.com>
2020-11-21 18:49:05 +0300

    GET_BOOK_QUERY

---
+++ client/src/components/BookDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BookDetails() {
+  return (
+    <div id="book__details">
+      <p>Output of book details here</p>
+    </div>
+  );
+}
+
+export default BookDetails;
